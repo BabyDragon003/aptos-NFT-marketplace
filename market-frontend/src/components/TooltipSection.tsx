77c4bd9a409d479ca8dbe630e3b78273d226cda8
BabyDragon003 <golddragon01234@gmail.com>
2020-12-10 16:21:52 +0900

    "2020-12-10 16:21"

--- market-frontend/src/components/TooltipSection.tsx
+++ market-frontend/src/components/TooltipSection.tsx
@@ -8,3 +8,13 @@ export function TooltipSection({ text }: TooltipSectionProps) {
           textOverflow: "ellipsis",
           display: "flex",
           wordBreak: "break-word",
+          minHeight: "3.6em",
+          maxHeight: "3.6em",
+          lineHeight: "1.8em",
+        }}
+      >
+        {text}
+      </p>
+    </div>
+  );
+}
